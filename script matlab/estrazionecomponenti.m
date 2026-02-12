78bbd9d57b49673d5c92ee7afff737c697d4884f
Cost4nza <154256834+Cost4nza@users.noreply.github.com>
2026-02-12 18:57:16 +0100

    created new vector-like txt original files

--- script matlab/estrazionecomponenti.m
+++ script matlab/estrazionecomponenti.m
@@ -1,11 +1,11 @@
 
-a=imread('lena32.jpg');
+a=imread('figures/immagini_originali/scacchiera.png');
 imshow(a);
-b=rgb2gray(a);
+b=im2gray(a);
 imshow(b);
 im=b';
 imc=reshape(im,1024,1);
- file_write=['lena32_matrix.txt'];
+ file_write=['file_txt_originali/originali_vettore/scacchiera_vettore.txt'];
     fid_T = fopen(file_write,'w');
     fprintf(fid_T,'%d\n',double(imc));
     fclose(fid_T);
